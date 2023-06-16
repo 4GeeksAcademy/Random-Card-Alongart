bdb827f90acd8798a6a2585d6963c3e86f1d96f6
Alon Gartenhaus <83038528+Alongarten@users.noreply.github.com>
2023-06-16 23:47:25 +0000

    initial commit, html layout done

--- src/app.js
+++ src/app.js
@@ -2,10 +2,20 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+const suites = ["spade", "club", "heart", "diamond"];
+let suite = suites[Math.floor(Math.random() * suites.length - 1)];
+
+const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+let cardFace = cardFaces[Math.floor(Math.random() * cardFaces.length - 1)];
+
+// if (suite == "spade" || "club") {
+//   cardFace.style.color = red;
+// } else {
+//   cardFace.style.color = black;
+// }
+
+console.log(cardFace, suite);
