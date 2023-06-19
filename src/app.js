cbb49fee6a386a900f9ff5200d52b110e4dee98b
Alon Gartenhaus <83038528+Alongarten@users.noreply.github.com>
2023-06-19 20:54:43 +0000

    added js functionality + button

--- src/app.js
+++ src/app.js
@@ -2,20 +2,48 @@
 import "bootstrap";
 import "./style.css";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
-};
+// window.onload = function() {
+//   changeSuit(suitClass, suitString);
+//   changeFace(cardFace, suitClass);
+//   console.log(cardFace, suitClass, suitString);
+// };
 
-const suites = ["spade", "club", "heart", "diamond"];
-let suite = suites[Math.floor(Math.random() * suites.length - 1)];
+const suits = { spade: "♠", club: "♣", heart: "♥", diamond: "♦" };
 
-const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-let cardFace = cardFaces[Math.floor(Math.random() * cardFaces.length - 1)];
+// let suitsArray = Object.keys(suits);
+// let suitClass = suitsArray[Math.floor(Math.random() * suitsArray.length)];
+// let suitString = suits[suitClass];
 
-// if (suite == "spade" || "club") {
-//   cardFace.style.color = red;
-// } else {
-//   cardFace.style.color = black;
-// }
+// const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+// let cardFace = cardFaces[Math.floor(Math.random() * cardFaces.length)];
 
-console.log(cardFace, suite);
+function changeFace() {
+  const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
+  let cardFace = cardFaces[Math.floor(Math.random() * cardFaces.length)];
+  let suitsArray = Object.keys(suits);
+  let suitClass = suitsArray[Math.floor(Math.random() * suitsArray.length)];
+  let suitString = suits[suitClass];
+  document.querySelector(".cardFace").innerHTML = cardFace;
+
+  if (suitClass === "spade" || suitClass === "club") {
+    document.querySelector(".cardFace").style.color = "black";
+  } else if (suitClass === "heart" || suitClass === "diamond") {
+    document.querySelector(".cardFace").style.color = "red";
+  }
+
+  document.querySelectorAll(".suit > p")[0].className = suitClass;
+  document.querySelectorAll(".suit > p")[0].innerHTML = suitString;
+  document.querySelectorAll(".suit > p")[1].className = suitClass;
+  document.querySelectorAll(".suit > p")[1].innerHTML = suitString;
+  console.log(suitString, suitClass, cardFace);
+}
+
+// function changeSuit() {}
+
+document.getElementById("newCard").addEventListener("click", function() {
+  // changeSuit();
+  changeFace();
+});
+
+// why console log shows 'spade' vs spade?
+// formatting (10 margin left?)
