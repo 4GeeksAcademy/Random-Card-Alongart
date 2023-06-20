af97563b18f6c485fe6b9fb90f8b9c1f5a8dca0c
Alon Gartenhaus <83038528+Alongarten@users.noreply.github.com>
2023-06-20 00:55:42 +0000

    finalized, neater

--- src/app.js
+++ src/app.js
@@ -2,20 +2,7 @@
 import "bootstrap";
 import "./style.css";
 
-// window.onload = function() {
-//   changeSuit(suitClass, suitString);
-//   changeFace(cardFace, suitClass);
-//   console.log(cardFace, suitClass, suitString);
-// };
-
 const suits = { spade: "♠", club: "♣", heart: "♥", diamond: "♦" };
-
-// let suitsArray = Object.keys(suits);
-// let suitClass = suitsArray[Math.floor(Math.random() * suitsArray.length)];
-// let suitString = suits[suitClass];
-
-// const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
-// let cardFace = cardFaces[Math.floor(Math.random() * cardFaces.length)];
 
 function changeFace() {
   const cardFaces = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"];
@@ -38,12 +25,10 @@ function changeFace() {
   console.log(suitString, suitClass, cardFace);
 }
 
-// function changeSuit() {}
 
 document.getElementById("newCard").addEventListener("click", function() {
-  // changeSuit();
+
   changeFace();
 });
 
-// why console log shows 'spade' vs spade?
-// formatting (10 margin left?)
+
